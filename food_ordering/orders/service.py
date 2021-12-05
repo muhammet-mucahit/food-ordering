4047b16e6723499c8ae424e9e73b190108726093
Mucahit Aktepe <mucahitaktepe@gmail.com>
2021-12-05 03:35:20 +0300

    Remove experimental code to ci pass

--- food_ordering/orders/service.py
+++ food_ordering/orders/service.py
@@ -1,12 +1,10 @@
 from django.db import transaction
 
-from food_ordering.libs.redis.service import RedisPubSubService
 from food_ordering.orders.models import Order, OrderFood
 
 
 class OrderService:
     orders_created_channel_name = "orders.created"
-    redis_pub_sub_service = RedisPubSubService(channel=orders_created_channel_name)
 
     @transaction.atomic()
     def create_order(self, user, restaurant, orderfood_set):
